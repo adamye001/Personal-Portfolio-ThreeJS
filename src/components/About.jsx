679f515594072442bcbbcc835d2840f263799f03
adam ye macbook <adamye001@gmail.com>
2023-06-05 17:20:11 +1000

    Finish work experience section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -43,7 +43,13 @@ const About = () => {
             <motion.p
                 variants={fadeIn("", "", 0.1, 1)}
                 className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
-                I'm a full-stack web developer.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
             </motion.p>
             <div className="mt-20 flex flex-wrap gap-10">
                 {services.map((service, index) => (
