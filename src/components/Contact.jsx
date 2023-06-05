9457f8c55422c1d811bedab1778cc5c31436612d
adam ye macbook <adamye001@gmail.com>
2023-06-05 23:16:27 +1000

    Finish the last stars canvas but need to update content on the website

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,9 +16,47 @@ const Contact = () => {
     });
     const [loading, setLoading] = useState(false);
 
-    const handleChange = (e) => {};
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setForm({ ...form, [name]: value });
+    };
 
-    const handleSubmit = (e) => {};
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setLoading(true);
+
+        emailjs
+            .send(
+                "service_2t38j9d",
+                "template_5batq83",
+                {
+                    from_name: form.name,
+                    to_name: "Adam",
+                    from_email: form.email,
+                    to_email: "adamye001@gmail.com",
+                    message: form.message,
+                },
+                "xdqgmZBly03fV7CNs"
+            )
+            .then(
+                () => {
+                    setLoading(false);
+                    alert(
+                        "Thank you. I will get back to you as soon as possible."
+                    );
+                    setForm({
+                        name: "",
+                        email: "",
+                        message: "",
+                    });
+                },
+                (error) => {
+                    setLoading(false);
+                    console.log(error);
+                    alert("Something went wrong.");
+                }
+            );
+    };
 
     return (
         <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
